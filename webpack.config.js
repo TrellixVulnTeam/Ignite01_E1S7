24b062b172d8164b7b9098d7afbe77737233256c
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-03-25 16:51:51 -0300

    Confirmar

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV != 'production';
+
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: isDevelopment? 'development':'production',
+    devtool: isDevelopment? 'eval-source-map': 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -11,8 +14,9 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+
     devServer: {
-        contentBase: path.resolve(__dirname, 'public', 'index.html'),
+         static: path.resolve(__dirname, 'public'),
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -25,7 +29,13 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader' , 'css-loader', 'sass-loader'],
             }
         ],
     }
+    
 };
